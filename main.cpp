faeca655b131a20bf30782853e3e984fb5e03a2c
David Stangl <david.stangl@student.hpi.de>
2017-02-03 15:07:09 +0100

    Add svg++, boost and libxml2 as dependencies

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <svgpp/svgpp.hpp>
+#include <libxml/parser.h>
 
 int main() {
+    LIBXML_TEST_VERSION
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }

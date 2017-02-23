c2ca26e2f44f537424595b7c430bc3f12cc585cc
David Stangl <david.stangl@student.hpi.de>
2017-02-23 15:11:02 +0100

    Implement document traversal

--- src/conversion.cpp
+++ src/conversion.cpp
@@ -3,38 +3,19 @@
 #include <svgpp/policy/xml/libxml2.hpp>
 #include <svgpp/svgpp.hpp>
 
-#include "utility.h"
+#include "parsing/context/factories.h"
+#include "parsing/context/svg.h"
+#include "parsing/policies.h"
 
-/**
- * List of elements which should be processed.
- */
-using ProcessedElements = Concat<
-    // Elements describing shapes
-    svgpp::traits::shape_elements,
-
-    // Supported structural elements
-    boost::mpl::set<svgpp::tag::element::svg, svgpp::tag::element::g>>;
-
-/**
- * List of attributes which should be processed.
- */
-using ProcessedAttributes = Concat<
-    // Attributes describing the shape of shape elements
-    svgpp::traits::shapes_attributes_by_element,
-
-    // Other attributes
-    boost::mpl::set<  // NOLINT not the stl set (no #include <set> needed)
-        svgpp::tag::attribute::transform>>;
-
-/**
- * Policy on how to handle paths (and other elements converted to paths).
- *
- * The `minimal` policy does all the conversions described at
- * http://svgpp.org/path.html#path-policy-concept. The conversion from arcs
- * to bézier curves is lossy. Because the silhouette can only plot arcs of
- * circles but not ellipses a conversion would be necessary anyway. Can be
- * replaced with another conversion if artifacts start to appear.
- */
-using PathPolicy = svgpp::policy::path::minimal;
-
-std::string convert(const ManagedXmlDoc& svgDoc) { return {}; }
+std::string convert(const ManagedXmlDoc& svgDoc) {
+    const Viewport global_viewport{};
+    SvgContext context{global_viewport};
+    xmlNodePtr root = xmlDocGetRootElement(svgDoc.get());
+    svgpp::document_traversal<
+        svgpp::processed_elements<ProcessedElements>,
+        svgpp::processed_attributes<ProcessedAttributes>,
+        svgpp::context_factories<ChildContextFactories>,
+        svgpp::path_policy<PathPolicy>,
+        svgpp::viewport_policy<ViewportPolicy>>::load_document(root, context);
+    return {};
+}
--- src/parsing/policies.h
+++ src/parsing/policies.h
@@ -1,6 +1,12 @@
 #ifndef SVG_CONVERTER_POLICIES_H
 #define SVG_CONVERTER_POLICIES_H
 
+#include <svgpp/policy/path.hpp>
+#include <svgpp/policy/viewport.hpp>
+#include <svgpp/traits/attribute_groups.hpp>
+#include <svgpp/traits/element_groups.hpp>
+
+#include "../utility.h"
 #include "context/svg.h"
 
 /**
@@ -24,5 +30,48 @@ struct DocumentTraversalControlPolicy {
         return true;
     }
 };
+
+/**
+ * List of elements which should be processed.
+ */
+using ProcessedElements = Concat<
+    // Elements describing shapes
+    svgpp::traits::shape_elements,
+
+    // Supported structural elements
+    boost::mpl::set<svgpp::tag::element::svg, svgpp::tag::element::g>>;
+
+/**
+ * List of attributes which should be processed.
+ */
+using ProcessedAttributes = Concat<
+    // Attributes describing the shape of shape elements
+    svgpp::traits::shapes_attributes_by_element,
+
+    // Viewport attributes for `<svg>` and `<pattern>`
+    svgpp::traits::viewport_attributes,
+
+    // Other attributes
+    boost::mpl::set<  // NOLINT not the stl set (no #include <set> needed)
+        svgpp::tag::attribute::transform>>;
+
+/**
+ * Policy on how to handle paths (and other elements converted to paths).
+ *
+ * The `minimal` policy does all the conversions described at
+ * http://svgpp.org/path.html#path-policy-concept. The conversion from arcs
+ * to bézier curves is lossy. Because the silhouette can only plot arcs of
+ * circles but not ellipses a conversion would be necessary anyway. Can be
+ * replaced with another conversion if artifacts start to appear.
+ */
+using PathPolicy = svgpp::policy::path::minimal;
+
+/**
+ * Handle transformations from viewport settings like normal transformations.
+ *
+ * Allows code reuse, because the reason for the transformation (viewport
+ * attributes or transform attribute) is not important for us.
+ */
+using ViewportPolicy = svgpp::policy::viewport::as_transform;
 
 #endif  // SVG_CONVERTER_POLICIES_H

76f237f7a84d4a5125bca51dd6903cb264d0ee8b
David Stangl <david.stangl@student.hpi.de>
2017-04-06 13:25:14 +0200

    Add clang format as a cmake target

---
+++ cmake/format.cmake
@@ -0,0 +1,14 @@
+# Convert all source and header paths to absolute
+foreach(REL_PATH ${CXX_SOURCE_AND_HEADER_FILES})
+    get_filename_component(ABS_PATH ${REL_PATH} ABSOLUTE)
+    list(APPEND CXX_ABSOLUTE_FORMAT_FILES ${ABS_PATH})
+endforeach()
+
+find_program(CLANG_FORMAT "clang-format")
+if (CLANG_FORMAT)
+    add_custom_target(format
+            COMMAND ${CLANG_FORMAT}
+            -i
+            -style=file
+            ${CXX_ABSOLUTE_FORMAT_FILES})
+endif()
--- format.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find src -iname '*.h' -or -iname '*.cpp' | xargs clang-format -style=file -i

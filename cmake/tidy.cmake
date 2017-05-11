3b2c47cc7840bf6a6d9c2cbe319cc76128f62ed0
David Stangl <david.stangl@student.hpi.de>
2017-05-11 14:40:39 +0200

    Enable clang-tidy for header files

--- cmake/tidy.cmake
+++ cmake/tidy.cmake
@@ -3,7 +3,7 @@
 find_program(CLANG_TIDY "clang-tidy")
 if (CLANG_TIDY)
     set_target_properties(${PROJECT_NAME} PROPERTIES
-            CXX_CLANG_TIDY ${CLANG_TIDY})
+            CXX_CLANG_TIDY "${CLANG_TIDY}")
 else()
     message(WARNING "clang-tidy not found!")
 endif()

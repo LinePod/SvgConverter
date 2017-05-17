6d7be990fae7e71e75cb9fa18265170b987c62fa
David Stangl <david.stangl@student.hpi.de>
2017-05-17 17:49:07 +0200

    Exported GPGL vectors now use a fixed precision.
    Fixes #10

--- src/parsing/gpgl_exporter.cpp
+++ src/parsing/gpgl_exporter.cpp
@@ -1,5 +1,7 @@
 #include "gpgl_exporter.h"
 
+#include <iomanip>
+
 /**
  * Factor to convert from millimeters to GPGL units.
  */
@@ -7,14 +9,21 @@ constexpr double kMillimeterToGpglFactor = 20;
 
 /**
  * Convert a point from millimeter based SVG space to GPGL space.
+ *
+ * Also rounds to a reasonable export precision.
  */
 Vector to_gpgl(Vector point) {
     // GPGL has a x axis that points to the right and a y axis that points down
-    return Vector{point(1), point(0)} * kMillimeterToGpglFactor;
+    Vector gpgl = Vector{point(1), point(0)} * kMillimeterToGpglFactor;
+    return gpgl.array().round().matrix();
 }
 
 GpglExporter::GpglExporter(std::ostringstream& out_stream)
-    : out_stream_{out_stream} {}
+    : out_stream_{out_stream} {
+    // Print double values without any decimal places.
+    // This works in tandem with the rounding done in `to_gpgl`.
+    out_stream_.get() << std::setprecision(0) << std::fixed;
+}
 
 void GpglExporter::plot(const Path& path,
                         const std::vector<double>& dasharray) {
